c1dda474fcca3928cde399c4ec0091add83b6eca
Akaash Kashyap <akaashkashyap@gmail.com>
2024-11-24 23:06:59 -0600

    committing nvim setup

--- init.lua
+++ init.lua
@@ -255,6 +255,20 @@ require('lazy').setup({
       },
     },
   },
+  {
+    'lervag/vimtex',
+    lazy = false, -- we don't want to lazy load VimTeX
+    -- tag = "v2.15", -- uncomment to pin to a specific release
+    init = function()
+      -- VimTeX configuration goes here, e.g.
+      vim.g.vimtex_view_method = 'zathura'
+      vim.g.vimtex_comiler_method = 'latexmk'
+      vim.g.vimtex_mappings_enabled = 1
+      vim.g.vimtex_compiler_latexmk = {
+        background = 1,
+      }
+    end,
+  },
 
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
